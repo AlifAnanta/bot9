69434d87bd19fec37b4b5ad44dd82c4e5addf44e
AlifAnanta <69768347+AlifAnanta@users.noreply.github.com>
2022-02-14 11:17:39 +0700

    :)

---
+++ Z1/grupbot.js
@@ -0,0 +1,11 @@
+const grupbot = (prefix) => { 
+	return `*Grup bot 1*
+https://chat.whatsapp.com/HbEeO3RlUosIYNX91xFhY2
+*Grup bot 2*
+https://chat.whatsapp.com/HbEeO3RlUosIYNX91xFhY2
+*Grup tes bot*
+https://chat.whatsapp.com/HbEeO3RlUosIYNX91xFhY2
+`
+}
+
+exports.grupbot = grupbot
